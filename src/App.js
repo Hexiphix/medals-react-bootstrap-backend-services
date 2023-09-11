487cf085e63f174375e6dfbade88b9343e016936
Hexiphix <jmolenda2002@gmail.com>
2023-09-11 10:57:50 -0500

    Change to Functional Components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-// Repository:  medals-b-react
-// Author:      Jeff Grissom
-// Version:     4.xx
-import React, { Component } from 'react';
+// Repository:  medals-b-react, medals-react-bootstrap
+// Authors:     Jeff Grissom, Justin Molenda
+// Version:     5j.xx
+import React, { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Badge from 'react-bootstrap/Badge';
@@ -14,119 +14,129 @@ import { PlusCircleFill } from 'react-bootstrap-icons';
 import Country from './components/Country';
 import './App.css';
 
-class App extends Component {
-  state = {
-    countries: [
-      { id: 1, name: 'United States', gold: 2, silver: 2, bronze: 3 },
-      { id: 2, name: 'China', gold: 3, silver: 1, bronze: 0 },
-      { id: 3, name: 'Germany', gold: 0, silver: 2, bronze: 2 },
-    ],
-    medals: [
-      { id: 1, name: 'gold' },
-      { id: 2, name: 'silver' },
-      { id: 3, name: 'bronze' },
-    ],
-    show: false,
-    newCountryName: ""
-  }
-  handleChange = (e) => this.setState({ [e.target.name]: e.target.value});
-  handleAdd = () => {
-    if (this.state.newCountryName.length > 0) {
-      const { countries } = this.state;
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [medals, setMedals] = useState([]);
+
+  const [show, setShow] = useState(false);
+  const [newCountryName, setNewCountryName] = useState("");
+
+  const handleAdd = () => {
+    if (newCountryName.length > 0) {
       const id = countries.length === 0 ? 1 : Math.max(...countries.map(country => country.id)) + 1;
-      const mutableCountries = [...countries].concat({ id: id, name: this.state.newCountryName, gold: 0, silver: 0, bronze: 0 });
-      this.setState({ countries: mutableCountries });
+      const mutableCountries = [...countries].concat({ id: id, name: newCountryName, gold: 0, silver: 0, bronze: 0 });
+      setCountries( mutableCountries );
     }
-    this.handleClose();
+    handleClose();
   }
-  handleDelete = (countryId) => {
-    const { countries } = this.state;
+
+  const handleDelete = (countryId) => {
     const mutableCountries = [...countries].filter(c => c.id !== countryId);
-    this.setState({ countries: mutableCountries });
+    setCountries( mutableCountries );
   }
-  handleIncrement = (countryId, medalName) => {
-    const countries = [ ...this.state.countries ];
-    const idx = countries.findIndex(c => c.id === countryId);
-    countries[idx][medalName] += 1;
-    this.setState({ countries: countries });
+
+  const handleIncrement = (countryId, medalName) => {
+    const mutableCountries = [ ...countries ];
+    const idx = mutableCountries.findIndex(c => c.id === countryId);
+    mutableCountries[idx][medalName] += 1;
+    setCountries( mutableCountries );
   }
-  handleDecrement = (countryId, medalName) => {
-    const countries = [ ...this.state.countries ];
-    const idx = countries.findIndex(c => c.id === countryId);
-    countries[idx][medalName] -= 1;
-    this.setState({ countries: countries });
+
+  const handleDecrement = (countryId, medalName) => {
+    const mutableCountries = [ ...countries ];
+    const idx = mutableCountries.findIndex(c => c.id === countryId);
+    mutableCountries[idx][medalName] -= 1;
+    setCountries(mutableCountries);
   }
-  getAllMedalsTotal() {
+
+  const getAllMedalsTotal = () => {
     let sum = 0;
-    this.state.medals.forEach(medal => { sum += this.state.countries.reduce((a, b) => a + b[medal.name], 0); });
+    medals.forEach(medal => { sum += countries.reduce((a, b) => a + b[medal.name], 0); });
     return sum;
   }
   
-  handleClose = () => {
-    this.setState({ show:false });
+  const handleClose = () => {
+    setShow(false);
   }
-  handleShow = () => {
-    this.setState({ newCountryName: "", show:true });
+  const handleShow = () => {
+    setShow(true);
+    setNewCountryName("");
   }
-  keyPress = (e) => {
-    (e.keyCode ? e.keyCode : e.which) == '13' && this.handleAdd();
+
+  const keyPress = (e) => {
+    (e.keyCode ? e.keyCode : e.which) == '13' && handleAdd();
   }
-  render() { 
-    return (
-      <React.Fragment>
-        <Modal onKeyPress={ this.keyPress } show={this.state.show} onHide={ this.handleClose }>
-          <Modal.Header closeButton>
-            <Modal.Title>New Country</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Country Name</Form.Label>
-              <Form.Control
-                type="text"
-                name="newCountryName"
-                onChange={ this.handleChange }
-                value={ this.state.newCountryName }
-                placeholder="enter name"
-                autoFocus
-                autoComplete='off'
-              />
-            </Form.Group>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={ this.handleClose }>
-              Close
-            </Button>
-            <Button variant="primary" onClick={ this.handleAdd }>
-              Save Changes
-            </Button>
-          </Modal.Footer>
-        </Modal>
-        <Navbar className="navbar-dark bg-dark">
-          <Container fluid>
-            <Navbar.Brand>
-              Olympic Medals
-              <Badge className="ml-2" bg="light" text="dark" pill>{ this.getAllMedalsTotal() }</Badge>
-            </Navbar.Brand>
-            <Button variant="outline-success" onClick={ this.handleShow }><PlusCircleFill /></Button>{' '}
-          </Container>
-        </Navbar>
+
+  useEffect(() => {
+    // Initial state data stored here
+    let mutableCountries = [
+      { id: 1, name: 'United States', gold: 2, silver: 2, bronze: 3 },
+      { id: 2, name: 'China', gold: 3, silver: 1, bronze: 0 },
+      { id: 3, name: 'Germany', gold: 0, silver: 2, bronze: 2 },
+    ];
+    let mutableMedals = [
+      { id: 1, name: 'gold' },
+      { id: 2, name: 'silver' },
+      { id: 3, name: 'bronze' },
+    ];
+    setCountries(mutableCountries);
+    setMedals(mutableMedals);
+  }, []);
+
+  return (
+    <React.Fragment>
+      <Modal onKeyPress={ keyPress } show={show} onHide={ handleClose }>
+        <Modal.Header closeButton>
+          <Modal.Title>New Country</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Country Name</Form.Label>
+            <Form.Control
+              type="text"
+              name="newCountryName"
+              onChange={ (e) => setNewCountryName(e.target.value) }
+              value={ newCountryName }
+              placeholder="enter name"
+              autoFocus
+              autoComplete='off'
+            />
+          </Form.Group>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={ handleClose }>
+            Close
+          </Button>
+          <Button variant="primary" onClick={ handleAdd }>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Navbar className="navbar-dark bg-dark">
         <Container fluid>
-          <Row>
-          { this.state.countries.map(country => 
-            <Col className="mt-3" key={ country.id }>
-              <Country  
-                country={ country } 
-                medals={ this.state.medals }
-                onDelete={ this.handleDelete }
-                onIncrement={ this.handleIncrement } 
-                onDecrement={ this.handleDecrement } />
-            </Col>
-          )}
-          </Row>
+          <Navbar.Brand>
+            Olympic Medals
+            <Badge className="ml-2" bg="light" text="dark" pill>{ getAllMedalsTotal() }</Badge>
+          </Navbar.Brand>
+          <Button variant="outline-success" onClick={ handleShow }><PlusCircleFill /></Button>{' '}
         </Container>
-      </React.Fragment>
-    );
-  }
+      </Navbar>
+      <Container fluid>
+        <Row>
+        { countries.map(country => 
+          <Col className="mt-3" key={ country.id }>
+            <Country  
+              country={ country } 
+              medals={ medals }
+              onDelete={ handleDelete }
+              onIncrement={ handleIncrement } 
+              onDecrement={ handleDecrement } />
+          </Col>
+        )}
+        </Row>
+      </Container>
+    </React.Fragment>
+  );
 }
  
 export default App;
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,45 +1,45 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Badge from 'react-bootstrap/Badge';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Medal from './Medal';
 import { TrashFill } from 'react-bootstrap-icons';
 
-class Country extends Component {
-  getMedalsTotal(country, medals) {
+const Country = (props) => {
+  const { country, medals, onIncrement, onDecrement, onDelete } = props;
+
+  const getMedalsTotal = () => {
     let sum = 0;
     medals.forEach(medal => { sum += country[medal.name]; });
     return sum;
   }
-  render() { 
-    const { country, medals, onIncrement, onDecrement, onDelete } = this.props;
-    return (
-      <Card style={{width:"22rem"}}>
-        <Card.Body>
-          <Card.Title className="d-flex justify-content-between">
-            <span>
-            { country.name }
-            <Badge bg="secondary" text="light" pill className="ml-2">
-              { this.getMedalsTotal(country, medals) }
-            </Badge>
-            </span>
-            <TrashFill onClick={() => onDelete(country.id)} className='icon-btn' style={{ color:'red' }} />
-          </Card.Title>
-          <ListGroup variant="flush">
-          { medals.map(medal =>
-            <ListGroup.Item className="d-flex justify-content-between" key={ medal.id }>
-              <Medal  
-                country={ country } 
-                medal={ medal } 
-                onIncrement={ onIncrement } 
-                onDecrement={ onDecrement } />
-            </ListGroup.Item>
-          ) }
-          </ListGroup>
-        </Card.Body>
-      </Card>
-    );
-  }
+
+  return (
+    <Card style={{width:"22rem"}} className='mx-auto'>
+      <Card.Body>
+        <Card.Title className="d-flex justify-content-between">
+          <span>
+          { country.name }
+          <Badge bg="secondary" text="light" pill className="ml-2">
+            { getMedalsTotal() }
+          </Badge>
+          </span>
+          <TrashFill onClick={() => onDelete(country.id)} className='icon-btn' style={{ color:'red' }} />
+        </Card.Title>
+        <ListGroup variant="flush">
+        { medals.map(medal =>
+          <ListGroup.Item className="d-flex justify-content-between" key={ medal.id }>
+            <Medal  
+              country={ country } 
+              medal={ medal } 
+              onIncrement={ onIncrement } 
+              onDecrement={ onDecrement } />
+          </ListGroup.Item>
+        ) }
+        </ListGroup>
+      </Card.Body>
+    </Card>
+  );
 }
 
 export default Country;
